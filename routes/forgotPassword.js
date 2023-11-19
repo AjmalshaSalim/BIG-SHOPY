6da82877fca913a3aed8add7789f1e0e4599177f
ajmal <ajmalaj8085@gmail.com>
2023-11-20 03:00:16 +0530

    forgot-pw

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const forgot_Password = express();
-const forgotP = require("../controllers/forgotPassword");
+const forgotP = require("../controllers/forgotpassword");
 
 forgot_Password.set('views', './views/users');
 
